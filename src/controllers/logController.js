41f4ef7923a47a3360c48819fd376f00fdd11d8e
ashab.tariq <ashab.tariq@hotmail.com>
2025-07-01 17:01:07 +0700

    Filter rules from ENV file, reduces unnecessary alerts to DB.

--- src/controllers/logController.js
+++ src/controllers/logController.js
@@ -2,6 +2,7 @@ import Log from '../models/Log.js';
 
 export const ingestLog = async (req, res) => {
   try {
+    const WHITELISTT_RULES = process.env.RULE_IDS.split(',');
     const { source, level, message, metadata, sourceIp } = req.body;
 
     console.log('📥 Incoming log:', { source, level, message, metadata });
@@ -10,13 +11,22 @@ export const ingestLog = async (req, res) => {
       return res.status(400).json({ message: 'source, level, and message are required' });
     }
 
-    const log = await Log.create({ source, level, message, metadata, sourceIp });
-
+    //CHECK IF RULE ID MATCHES LIST IN ENV
+    if(WHITELISTT_RULES.includes(metadata.rule.id)){
+      const log = await Log.create({ source, level, message, metadata, sourceIp });
+      res.status(201)
+    }
+    else{
+      console.log(`❌  Rule Discarded: [${level.toUpperCase()}] from ${source} - ${message}`);
+      res.status(405) //RULE DISCARD STATUS
+    }
+    
+    
     if (['error', 'critical'].includes(level)) {
       console.log(`🔔 Alert: [${level.toUpperCase()}] from ${source} - ${message}`);
     }
 
-    res.status(201).json(log);
+    
   } catch (err) {
     console.error('❌ DB Error:', err.message);
     res.status(500).json({ message: err.message });
